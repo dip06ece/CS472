ad6c0a5e818a7d7f2d2fc085fabb88687f8e20ac
Dip Ranjon Das <dip06ece@gmail.com>
2024-07-22 22:58:11 -0400

    Problem 1 updated

--- L6/script.js
+++ L6/script.js
@@ -1,2 +1,18 @@
-const x = 5;
-console.log(!x);
+/* Exercise 1:
+Define a filter function on the String object. The function accepts an array of strings that
+specifies a list of banned words. The function returns the string after removing all the
+banned words.
+Example:
+console.log("This house is not nice!".filter('not'));
+Output: "This house is nice!"
+*/
+String.prototype.filter = function(bannedWords) {
+    let words = this.split(' ');
+    let filteredWords = words.filter(word => !bannedWords.includes(word));
+    return filteredWords.join(' ');
+};
+
+function main() {
+    // Example usage:
+    console.log("This house is not nice!".filter(['not']));
+}
