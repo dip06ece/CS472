ab547e55873184a6b3cea287150f54f0682285b9
Dip Ranjon Das <dip06ece@gmail.com>
2024-08-04 11:15:07 -0400

    Maze script updated

---
+++ L8/maze.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    let isWin = true;
+    let isStart = false;
+
+    $("#start").click(function(){
+        //isWin = true;
+        if(!isStart){
+            isStart = true;
+            $('#maze > div.boundary').removeClass('youlose');
+            $("#status").text('Started!...');
+        }
+    });
+
+    $("#end").mouseover(function(){
+        if(isStart){
+            if(isWin){
+                $("#status").text('You win!!!');
+
+            }
+            else{
+                $("#status").text("You lost. :(");
+            }
+        }
+        isStart = false;
+    });
+
+    $('#maze > div.boundary').mouseover(function(){
+        if(isStart){
+            $('#maze > div.boundary').addClass('youlose');
+            $("#status").text("Game Over!!! :(");
+            isWin = false;
+            isStart = false;
+        }
+    });
+
+    $("body, div#maze, div#maze div").mouseover(function(e){
+        if (isStart) {
+            if (this.className != "boundary" && this.id != "maze" && this.nodeName != "DIV") {
+                //disable cheating
+                $("#status").text("You lost. :(");
+                $("#maze > div.boundary").addClass("youlose");
+                isStart = false;
+            }
+        }
+        e.stopPropagation();
+    })
+});
