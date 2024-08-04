5b56e5df973984c54c4e3c76cff6d7439d9137a0
Dip Ranjon Das <dip06ece@gmail.com>
2024-08-04 11:37:59 -0400

    JQuery file added in local

--- L8/maze.js
+++ L8/maze.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    let isWin = true;
+    let isWin = false;  // Default to false at start
     let isStart = false;
 
     $("#start").click(function(){
-        isWin = true;
         if(!isStart){
             isStart = true;
+            isWin = true;  // Assume player wins until they lose
             $('#maze > div.boundary').removeClass('youlose');
             $("#status").text('Started!...');
         }
@@ -15,35 +15,32 @@ $(document).ready(function(){
         if(isStart){
             if(isWin){
                 $("#status").text('You win!!!');
-
-            }
-            else{
+            } else {
                 $("#status").text("You lost. :(");
-                isWin = false;
             }
+            isStart = false;  // End game after interacting with the end
         }
-        isStart = false;
     });
 
     $('#maze > div.boundary').mouseover(function(){
         if(isStart){
             $('#maze > div.boundary').addClass('youlose');
             $("#status").text("Game Over!!! :(");
-            isWin = false;
-            isStart = false;
+            isWin = false;  // Player loses if they hit the boundary
+            isStart = false;  // End game
         }
     });
 
     $("body, div#maze, div#maze div").mouseover(function(e){
         if (isStart) {
             if (this.className != "boundary" && this.id != "maze" && this.nodeName != "DIV") {
-                //disable cheating
+                // Disable cheating by checking if the mouse is outside the maze and boundary
                 $("#status").text("You lost. :(");
                 $("#maze > div.boundary").addClass("youlose");
-                isWin =false;
-                isStart = false;
+                isWin = false;
+                isStart = false;  // End game
             }
         }
-        e.stopPropagation();
-    })
+        e.stopPropagation();  // Prevent event bubbling
+    });
 });
